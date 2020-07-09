b22a1fe448137740fae0d1015a2ce7ea2378f122
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2020-07-09 15:53:54 +0300

    Update C++ class

---
+++ Source/BattleOfPyvlandia/Private/Character/Player/PlayerCharacter.cpp
@@ -0,0 +1,34 @@
+#include "..//Public/Character/Player/PlayerCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetMesh());
+
+	TPS_Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("TPS_Camera"));
+	TPS_Camera->SetupAttachment(SpringArm);
+
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/BattleOfPyvlandia/Public/Character/Player/PlayerCharacter.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class BATTLEOFPYVLANDIA_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	APlayerCharacter();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Player)
+		class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Player)
+		class UCameraComponent* TPS_Camera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Player)
+		class UCameraComponent* FPS_Camera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Player)
+		class USkeletalMeshComponent* FPS_Arms;
+
+};
