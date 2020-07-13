3e499d0f10ae91ff2b3b99c71e6d46babd789bf6
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2020-07-13 17:28:38 +0300

    Base damage reaction

--- Source/BattleOfPyvlandia/Components/HealthComponent.cpp
+++ Source/BattleOfPyvlandia/Components/HealthComponent.cpp
@@ -1,34 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "HealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
 
-// Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
-
-// Called when the game starts
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
 }
 
+void UHealthComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-// Called every frame
+	DOREPLIFETIME(UHealthComponent, MaxHealth);
+	DOREPLIFETIME(UHealthComponent, Health);
+}
+
 void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
 
+void UHealthComponent::GetDamage(float Damage)
+{
+	Health -= Damage;
+	Health = FMath::Max(0.f, Health);
+	if (Health <= 0.f) OnHealthEnded.Broadcast();
+}
+
+ 
--- Source/BattleOfPyvlandia/Components/HealthComponent.h
+++ Source/BattleOfPyvlandia/Components/HealthComponent.h
@@ -1,28 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealthEnded);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLEOFPYVLANDIA_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
+
 	UHealthComponent();
 
 protected:
-	// Called when the game starts
+
 	virtual void BeginPlay() override;
 
+	void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const;
+
 public:	
-	// Called every frame
+
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = Health)
+		float MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = Health)
+		float Health;
+
+	UPROPERTY(BlueprintAssignable, Category = Health)
+		FHealthEnded OnHealthEnded;
+
+	UFUNCTION(BlueprintCallable, Category = Health)
+		void GetDamage(float Damage);
 };
